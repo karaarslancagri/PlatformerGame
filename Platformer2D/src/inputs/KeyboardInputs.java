739c5f6c2881d2067b16ba7041bc5b2847731dc7
Çağrı Karaarslan <karaarslancagri1@gmail.com>
2023-12-30 19:51:43 +0300

    Fixed player animations

--- Platformer2D/src/inputs/KeyboardInputs.java
+++ Platformer2D/src/inputs/KeyboardInputs.java
@@ -4,8 +4,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import main.GamePanel;
-import main.Game;
-import static utilz.Constants.Directions.*;
 
 public class KeyboardInputs implements KeyListener{
 	
@@ -21,36 +19,39 @@ public class KeyboardInputs implements KeyListener{
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
-		switch(e.getKeyCode()) {
-		case KeyEvent.VK_W:	
-		case KeyEvent.VK_A:			
-		case KeyEvent.VK_S:			
-		case KeyEvent.VK_D:
-			gamePanel.setMoving(false);
-			break;
-		}
-		
-	}
-
-	@Override
 	public void keyReleased(KeyEvent e) {	
 		switch(e.getKeyCode()) {
 		case KeyEvent.VK_W:
-			gamePanel.setDirection(UP);
+			gamePanel.getGame().getPlayer().setUp(false);
 			break;
 		case KeyEvent.VK_A:
-			gamePanel.setDirection(LEFT);
+			gamePanel.getGame().getPlayer().setLeft(false);
 			break;
-		case KeyEvent.VK_S:
-			gamePanel.setDirection(DOWN);
+		case KeyEvent.VK_S:	
+			gamePanel.getGame().getPlayer().setDown(false);
 			break;
 		case KeyEvent.VK_D:
-			gamePanel.setDirection(RIGHT);
+			gamePanel.getGame().getPlayer().setRight(false);
 			break;
-			
 		}
-		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:
+			gamePanel.getGame().getPlayer().setUp(true);
+			break;
+		case KeyEvent.VK_A:
+			gamePanel.getGame().getPlayer().setLeft(true);
+			break;
+		case KeyEvent.VK_S:	
+			gamePanel.getGame().getPlayer().setDown(true);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.getGame().getPlayer().setRight(true);
+			break;
+		}
 	}
 
 }
