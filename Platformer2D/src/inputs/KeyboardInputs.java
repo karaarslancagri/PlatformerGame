31a9a4279daa50f12289ebec5f544536e51dade2
Çağrı Karaarslan <karaarslancagri1@gmail.com>
2023-12-30 17:54:54 +0300

    Created player animations

--- Platformer2D/src/inputs/KeyboardInputs.java
+++ Platformer2D/src/inputs/KeyboardInputs.java
@@ -4,6 +4,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import main.GamePanel;
+import main.Game;
+import static utilz.Constants.Directions.*;
 
 public class KeyboardInputs implements KeyListener{
 	
@@ -20,7 +22,14 @@ public class KeyboardInputs implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:	
+		case KeyEvent.VK_A:			
+		case KeyEvent.VK_S:			
+		case KeyEvent.VK_D:
+			gamePanel.setMoving(false);
+			break;
+		}
 		
 	}
 
@@ -29,16 +38,16 @@ public class KeyboardInputs implements KeyListener{
 		
 		switch(e.getKeyCode()) {
 		case KeyEvent.VK_W:
-			gamePanel.changeYDelta(-5);
+			gamePanel.setDirection(UP);
 			break;
 		case KeyEvent.VK_A:
-			gamePanel.changeXDelta(-5);
+			gamePanel.setDirection(LEFT);
 			break;
 		case KeyEvent.VK_S:
-			gamePanel.changeYDelta(5);
+			gamePanel.setDirection(DOWN);
 			break;
 		case KeyEvent.VK_D:
-			gamePanel.changeXDelta(5);
+			gamePanel.setDirection(RIGHT);
 			break;
 			
 		}
--- Platformer2D/src/main/Game.java
+++ Platformer2D/src/main/Game.java
@@ -46,5 +46,8 @@ public class Game implements Runnable{
 				}
 				
 			}
+			
+		
+			
 	}
 }
